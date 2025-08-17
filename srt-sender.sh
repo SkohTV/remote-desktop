71eabc12a18f542f6899a682fb0030e482ed2f7b
SkohTV <qlpth+github@proton.me>
2025-08-17 04:02:18 +0200

    the cool script is wip, but working

--- srt-sender.sh
+++ srt-sender.sh
@@ -1,12 +1,43 @@
 #!/usr/bin/env bash
 
-IP=$1
-PORT=4200
+PORT=4200 # Good default
+
+WL_DEVICE=$(\ls /dev/dri/card*)
+PL_DEVICE=$(pactl list sinks | grep -A1 "State: RUNNING" | grep 'Name:' | cut -d':' -f2 | xargs)
 
 
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --ip)
+      IP="$2"
+      shift ; shift ;;
+    --port)
+      PORT="$2"
+      shift ; shift ;;
+    --wl_device)
+      WL_DEVICE="$2"
+      shift ; shift ;;
+    --pl_device)
+      PL_DEVICE="$2"
+      shift ; shift ;;
+    -*)
+      echo "Unknown option $1"
+      exit 1 ;;
+    *)
+      echo "Wrong syntax"
+      exit 1 ;;
+  esac
+done
+
+
+
+# The big command
+# To find 
 ffmpeg \
-  -f pulse -i bluez_output.40_58_99_49_5D_85.1.monitor \
-  -device /dev/dri/card1 -f kmsgrab -i - \
+  -f pulse -i "${PL_DEVICE}.monitor" \
+  -device "${WL_DEVICE}" -f kmsgrab -i - \
   -vf 'hwmap=derive_device=vaapi,scale_vaapi=format=nv12' \
-  -c:v h264_vaapi  -qp 24 -r 60 \
+  -c:v h264_vaapi \
+  -qp 24 -r 60 \
   -f mpegts "srt://${IP}:${PORT}"
+
